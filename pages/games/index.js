d628b382fc476e62f98143d77629dc721f464405
Wesley Vance <wslyvnce@gmail.com>
2023-05-23 21:19:46 -0400

    added game form to new game page for games, added button to route to form from games page

--- pages/games/index.js
+++ pages/games/index.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { useRouter } from 'next/router';
 import GameCard from '../../components/game/GameCard';
 import { getGames } from '../../utils/data/gameData';
 
 function Home() {
   const [games, setGames] = useState([]);
+  const router = useRouter();
 
   useEffect(() => {
     getGames().then((data) => setGames(data));
@@ -12,6 +15,12 @@ function Home() {
   return (
     <article className="games">
       <h1>Games</h1>
+      <Button onClick={() => {
+        router.push('/games/new');
+      }}
+      >
+        Register New Game
+      </Button>
       {games.map((game) => (
         <section key={`game--${game.id}`} className="game">
           <GameCard title={game.title} maker={game.maker} numberOfPlayers={game.number_of_players} skillLevel={game.skill_level} />
