a83b7bec15a1fac0047178b95d6a37c4c61596f1
Wesley Vance <wslyvnce@gmail.com>
2023-05-23 21:14:49 -0400

    built react form

---
+++ components/game/GameForm.js
@@ -0,0 +1,113 @@
+import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import { createGame, getGameTypes } from '../../utils/data/gameData';
+
+const initialState = {
+  skillLevel: 1,
+  numberOfPlayers: 0,
+  title: '',
+  maker: '',
+  gameTypeId: 0,
+};
+
+const GameForm = ({ user }) => {
+  const [gameTypes, setGameTypes] = useState([]);
+  /*
+  Since the input fields are bound to the values of
+  the properties of this state variable, you need to
+  provide some default values.
+  */
+  const [currentGame, setCurrentGame] = useState(initialState);
+  const router = useRouter();
+
+  useEffect(() => {
+    // TODO: Get the game types, then set the state
+    getGameTypes().then((data) => setGameTypes(data));
+  }, []);
+
+  const handleChange = (e) => {
+    // TODO: Complete the onChange function
+    const { name, value } = e.target;
+    setCurrentGame((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    // Prevent form from being submitted
+    e.preventDefault();
+
+    const game = {
+      maker: currentGame.maker,
+      title: currentGame.title,
+      numberOfPlayers: Number(currentGame.numberOfPlayers),
+      skillLevel: Number(currentGame.skillLevel),
+      gameType: Number(currentGame.gameTypeId),
+      userId: user.uid,
+    };
+
+    // Send POST request to your API
+    createGame(game).then(() => router.push('/games'));
+  };
+
+  return (
+    <>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label>Title</Form.Label>
+          <Form.Control name="title" type="text" required value={currentGame.title} onChange={handleChange} />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Maker</Form.Label>
+          <Form.Control name="maker" type="text" required value={currentGame.maker} onChange={handleChange} />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Number of Players</Form.Label>
+          <Form.Control name="numberOfPlayers" type="number" required value={currentGame.numberOfPlayers} onChange={handleChange} />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Skill Level</Form.Label>
+          <Form.Control name="skillLevel" type="number" required value={currentGame.skillLevel} onChange={handleChange} />
+        </Form.Group>
+        <FloatingLabel controlId="floatingSelect" label="gametype">
+          <Form.Select
+            placeholder="Select Game Type:"
+            aria-label="Game Type"
+            name="gameType"
+            onChange={handleChange}
+            className="mb-3"
+            value={currentGame.gameType}
+            required
+          >
+            <option value="">Select Game Type</option>
+            {gameTypes.map((gameType) => (
+              <option
+                key={gameType.id}
+                value={gameType.id}
+              >
+                {gameType.label}
+              </option>
+            ))}
+          </Form.Select>
+        </FloatingLabel>
+        {/* TODO: create the rest of the input fields */}
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
+};
+
+GameForm.propTypes = {
+  user: PropTypes.shape({
+    uid: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default GameForm;
